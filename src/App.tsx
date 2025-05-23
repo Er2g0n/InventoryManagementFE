9989d249981322e66d00618fbb3c3193daf83bfb
Bao <nguyengiabao91753@gmail.com>
2025-05-23 13:52:32 +0700

    feat: congfig redux -> just for run project

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
 import { HelmetProvider } from "react-helmet-async";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import store from "./store/store";
+
 
 const router = createBrowserRouter(routes);
 
---
+++ src/store/store.ts
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import {
+  TypedUseSelectorHook,
+  useDispatch as reduxUseDispatch,
+  useSelector as reduxUseSelector,
+} from 'react-redux';
+import rootReducer from './rootReducer';
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+const store = configureStore({ reducer: rootReducer });
+
+// Infer the `AppDispatch` types from the store itself
+export type AppDispatch = typeof store.dispatch;
+
+export const useDispatch = () => reduxUseDispatch<AppDispatch>();
+export const useSelector: TypedUseSelectorHook<RootState> = reduxUseSelector;
+
+export default store;
