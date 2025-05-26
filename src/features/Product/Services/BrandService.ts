bfb219a3babaa44371421141bff13ef8d7b7e04b
Eragon <hnguyenngoc.h@gmail.com>
2025-05-26 17:35:54 +0700

    chore: fix syntax

--- src/features/Product/Services/BrandService.ts
+++ src/features/Product/Services/BrandService.ts
@@ -1,30 +1,32 @@
 "use client";
 import { fetchClient } from "@/api/fetchClient";
 import { ResultService } from "@/types/Base/ResultService";
-import { Brand } from "@/types/ProductClassification/Brand/Brand";
+import { Brand } from "@/types/MasterData/Product/ProductClassification";
 
 let brandCache: Brand[] | null = null;
 
-export async function getAllBrands(
-  options: { cache: boolean } = { cache: true }
+export async function getAllBrands (
 ): Promise<ResultService<Brand[]>> {
   const response = await fetchClient<Brand[]>("GET", "Brand");
 
   return response;
 }
 
-export async function getBrandById(id: number): Promise<Brand> {
+export async function getBrandById (id: number): Promise<Brand> {
   const response = await fetchClient<Brand>("GET", `Brand/${id}`);
 
+  if (!response.data) {
+    throw new Error(`Brand with id ${id} not found.`);
+  }
   return response.data;
-
+  
 }
 
-export async function deleteBrand(id: number): Promise<void> {
+export async function deleteBrand (id: number): Promise<void> {
   await fetchClient("DELETE", "Brand", { data: id });
 }
 
-export async function getBrandByCode(
+export async function getBrandByCode (
   brandCode: string
 ): Promise<ResultService<Brand>> {
 
@@ -34,10 +36,11 @@ export async function getBrandByCode(
     `Brand/brandCode/${brandCode}`
     
   );
+
   return response;
 }
 
-export async function saveBrandByDapper(
+export async function saveBrandByDapper (
   brand: Brand
 ): Promise<ResultService<Brand>> {
   const response = await fetchClient<Brand, Brand>(
@@ -50,6 +53,7 @@ export async function saveBrandByDapper(
 
   if (result.code === "0" && result.data && brandCache) {
     const index = brandCache.findIndex((b) => b.brandCode === brand.brandCode);
+
     if (index !== -1) {
       brandCache[index] = result.data;
     } else {
@@ -59,15 +63,18 @@ export async function saveBrandByDapper(
   return result;
 }
 
-export async function deleteBrandByDapper(
+export async function deleteBrandByDapper (
   brandCode: string
 ): Promise<ResultService<void>> {
-  const response = await fetchClient<void>("DELETE", `Brand/DeleteByDapper`, {
+  const response = await fetchClient<void>("DELETE", "Brand/DeleteByDapper", {
+
     params: { brandCode }
+    
   });
+
   return response;
 }
 
-export function clearBrandCache(): void {
+export function clearBrandCache (): void {
   brandCache = null;
 }
