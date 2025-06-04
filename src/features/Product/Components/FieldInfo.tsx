a59de0d55b45b00371d45038ef77703a69349f66
Bao <nguyengiabao91753@gmail.com>
2025-06-05 01:22:07 +0700

    feat: Show err Component

---
+++ src/features/Product/Components/FieldInfo.tsx
@@ -0,0 +1,14 @@
+import { AnyFieldApi } from "@tanstack/react-form";
+
+function FieldInfo({ field }: { field: AnyFieldApi }) {
+    return (
+        <>
+            {field.state.meta.errors.length > 0 ? (
+                <span style={{ color: "red" }}>{field.state.meta.errors.map((err) => err.message).join(',')}</span>
+            ) : null}
+           
+        </>
+    )
+}
+
+export default FieldInfo;
