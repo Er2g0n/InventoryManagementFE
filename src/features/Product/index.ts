ce05b96f48e62b89ed82cc6e793f984e6ed9a0b3
Bao <nguyengiabao91753@gmail.com>
2025-06-05 01:21:13 +0700

    feat: add export ColorSlide

--- src/features/Product/index.ts
+++ src/features/Product/index.ts
@@ -2,3 +2,4 @@ export { default as ProductRoutes} from "./routes";
 export { default as productSlice} from  "./ProductSlice"
 export { default as productCategorySlice } from "./store/ProductCategory/index";
 export { default as productTypeSlice } from "./store/ProductType/index";
+export { default as colorSlice } from "./store/Color/index";
