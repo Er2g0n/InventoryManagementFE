5dc5365d4f43459b57bae04c43e5c5c41f514bf7
Bao <nguyengiabao91753@gmail.com>
2025-06-05 01:19:32 +0700

    feat: Color Service

---
+++ src/features/Product/Services/ProductColorService.ts
@@ -0,0 +1,71 @@
+import httpMethod from "@/constants/httpMethod";
+import { fetchClient } from "@/api/fetchClient";
+import { ResultService } from "@/types/Base/ResultService";
+import { Color } from "@/types/MasterData/Product/ProductProperties";
+
+export async function getAllColor (): Promise<ResultService<Color[]>> {
+  const response = await fetchClient<Color[]>(
+    httpMethod.GET,
+    "Color",
+  );
+  
+  if (response.code == "-1") {
+    response.message = "Failed to fetch data";
+    return response;
+  }
+  response.code = "0";
+  return response;
+}
+
+export async function saveColor (
+  color: Color
+): Promise<ResultService<Color>> {
+  
+  const response = await fetchClient<Color, Color>(
+    httpMethod.POST,
+    "Color/SaveByDapper",
+    color
+  );
+  
+  if (response.code == "-1") {
+    response.message = "Failed to save this color";
+    return response;
+  }
+  response.message = "Saved successfully";
+
+  return response;
+}
+
+export async function getColorByCode (colorCode: string): Promise<ResultService<Color>> {
+  const response = await fetchClient<Color, string>(
+    httpMethod.GET,
+    `Color/${colorCode}`,
+  );
+  
+  if (response.code == "-1") {
+    response.message = "Failed to get data";
+    return response;
+  }
+  response.code = "0";
+  // response.message = "Color retrieved successfully";
+
+  return response;
+}
+
+export async function deleteColor (
+  colorCode: string
+): Promise<ResultService<string>> {
+  const response = await fetchClient<string, string>(
+    httpMethod.DELETE,
+    `Color/DeleteByDapper/${encodeURIComponent(colorCode)}`  
+  );
+ 
+
+  if (response.code == "-1") {
+    response.message = "Failed to delete this color";
+    return response;
+  }
+  response.code = "0";
+  response.message = "Color deleted successfully";
+  return response;
+}
