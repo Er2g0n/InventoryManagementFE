eef0826647babd4a2d6a9ba2c54ce5fc3b7999fa
Eragon <hnguyenngoc.h@gmail.com>
2025-06-28 00:42:03 +0700

    chore: refactor code

--- src/features/Warehouse/Pages/Main.tsx
+++ src/features/Warehouse/Pages/Main.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
 export default function MainPage () {
   return (
     <div>
-      <h1>Ware house main page</h1>
+      <h1>Inventory Management main page</h1>
     </div>
   );
 }
