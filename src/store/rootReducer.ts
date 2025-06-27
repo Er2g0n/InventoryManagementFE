bf1bab20e997bd4eb2a942a180a291dcd70e3089
Eragon <hnguyenngoc.h@gmail.com>
2025-06-28 00:47:56 +0700

    chore: add slice to file rootReducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,14 +1,20 @@
-import { productSlice, productTypeSlice, productCategorySlice, colorSlice, TransactionTypeSlice, MaterialSlice, brandSlice } from "@features/Product";
+import { warehouseSlice } from "@features/Inventory";
+import { productSlice, productTypeSlice, productCategorySlice, colorSlice, TransactionTypeSlice, MaterialSlice, brandSlice, vehicleModelSlice } from "@features/Product";
 import { combineReducers } from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
+  // Product domain
   product: productSlice,
   productCategory: productCategorySlice,
   productType: productTypeSlice,
   color: colorSlice,
   transactionType: TransactionTypeSlice,
   material: MaterialSlice,
-  brand: brandSlice
+  brand: brandSlice,
+  vehicleModel: vehicleModelSlice,
+
+  // Inventory domain
+  warehouse: warehouseSlice
 });
 
 export default rootReducer;
